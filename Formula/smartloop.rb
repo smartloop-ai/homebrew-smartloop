4546c66c387b876dc94c7cf30bfdbf2c6f68c926
Mehfuz <mehfuz@gmail.com>
2025-05-14 20:17:29 -0700

    v1.0.1 macos build

---
+++ Formula/smartloop.rb
@@ -0,0 +1,54 @@
+class Smartloop < Formula
+  desc "Smartloop framework for building LLM agents and tools"
+  homepage "https://smartloop.ai"
+  version "1.0.1"
+  license "Apache-2.0"
+
+  # Prevent Homebrew from rewriting @rpath in the PyInstaller bundle's dylibs.
+  skip_clean :all
+
+  on_macos do
+    url "https://storage.googleapis.com/smartloop-gcp-us-east-releases/1.0.1/darwin/arm64/slp.tar.gz"
+    sha256 "48ccf608d643b8ac5afb9683a8dc1f7d9bb3677d444d822feef9971f8ec73d5c"
+  end
+
+  on_linux do
+    url "https://storage.googleapis.com/smartloop-gcp-us-east-releases/1.0.1/linux/amd64/slp.tar.gz"
+    sha256 "5cec6f66e23de047c210337ba83595c6da13727eac4927e2ac6f7d0da16a2bd2"
+  end
+
+  def install
+    # The tarball extracts to a slp/ directory containing the PyInstaller binary
+    # and its _internal/ dependencies. Install the whole directory to libexec.
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"slp"
+  end
+
+  def post_install
+    (var/"log").mkpath
+  end
+
+  service do
+    run [opt_bin/"slp", "server", "start"]
+    keep_alive true
+    log_path var/"log/smartloop.log"
+    error_log_path var/"log/smartloop.log"
+    working_dir opt_libexec
+  end
+
+  def caveats
+    <<~EOS
+      To start the Smartloop service:
+        brew services start smartloop
+
+      To manage the service:
+        brew services stop smartloop
+        brew services restart smartloop
+    EOS
+  end
+
+  test do
+    assert_predicate bin/"slp", :exist?
+    assert_predicate bin/"slp", :executable?
+  end
+end
